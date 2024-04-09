9b004debe267de41b350b6ba7d6d3f4ee41d2b8c
Ladyka Alisa <ladykaalisa@gmail.com>
2024-04-09 11:50:59 +0300

    done test3

--- cypress/e2e/form-tests/auth-form.cy.js
+++ cypress/e2e/form-tests/auth-form.cy.js
@@ -3,10 +3,29 @@
 describe('example to-do app', () => {
  
     it ("test 1", () => {
-      cy.visit('https://the-internet.herokuapp.com/login');
+      cy.visit('/login');
       cy.get('input#username').type('tomsmith');
       cy.get('input#password').type('Super');
       cy.get('button[type="submit"]').click();
       cy.get('div#flash').should('have.text' , '\n            Your password is invalid!\n            ×\n          ');
       })
-    })
+     
+    it ("test 2", () => {
+        cy.visit('/login');
+        cy.get('input#username').type('tom');
+        cy.get('input#password').type('Super');
+        cy.get('button[type="submit"]').click();
+        cy.get('div#flash').should('have.text' , '\n            Your password is invalid!\n            ×\n          ');
+        })
+
+        it ("test 3", () => {
+            cy.visit ('https://qauto.forstudy.space/' , {
+         auth: {
+            username: 'guest',
+            password: 'welcome2qauto' ,
+        },
+         })
+            cy.get('button.btn-primary').should('have.text', 'Sign up');
+            })
+
+        })
